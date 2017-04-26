c0263d1eec61b7727ba44c51763f632d251a5721
tanyamehta <tanya.mehta24@gmail.com>
2017-04-26 14:44:49 +0530

    Update tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -5,16 +5,17 @@ CREATE DATABASE tournament;
 \c tournament;
 
 
-CREATE TABLE matchtournament(
-    m_id SERIAL PRIMARY KEY,
-    loser INTEGER,
-    winner INTEGER
-);
-
-
 CREATE TABLE playertournament(
     p_id SERIAL PRIMARY KEY,
     name TEXT
 ); 
+
+CREATE TABLE matchtournament(
+    m_id SERIAL PRIMARY KEY,
+    loser INTEGER,
+    winner INTEGER,
+    FOREIGN KEY(winner) REFERENCES playertournament(p_id),
+    FOREIGN KEY(loser) REFERENCES playertournament(p_id)
+);
 
 CREATE VIEW rankings AS SELECT p_id,name, (SELECT count(*) FROM matchtournament WHERE playertournament.p_id = matchtournament.winner) AS wins, (SELECT count(*) FROM matchtournament WHERE playertournament.p_id IN (matchtournament.loser,matchtournament.winner)) AS played FROM playertournament ORDER BY wins DESC;
